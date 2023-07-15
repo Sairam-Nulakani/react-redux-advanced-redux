de31dbd1403542b14a08a424d8b6100530365e20
“Sairam-Nulakani” <sairamnulakani4@gmail.com>
2023-07-15 16:49:01 +0530

    using redux in counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,22 @@
 import "./counter.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   return (
     <div className="container">
       <h1>Redux Counter</h1>
-      <div className="value">--Counter Value</div>
+      <div className="value">{counter}</div>
       <div>
-        <button>Decrement</button>
-        <button>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
       </div>
       <button>Toggle Counter</button>
     </div>
